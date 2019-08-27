a5564cd3c43d0e3d9365d808504ee77ae1a25dee
Antenor Lopes Vieira <antenorlopes.d@gmail.com>
2019-08-26 23:23:58 -0500

    Tela de Login

--- vendor/loja/php-classes/src/Page.php
+++ vendor/loja/php-classes/src/Page.php
@@ -12,6 +12,8 @@
 		private $options = [];
 
 		private $defaults = [
+			"header"=>true,
+			"footer"=>true,
 			"data"=>[]
 		];
 
@@ -31,7 +33,7 @@
 
 			$this->setData($this->options["data"]);
 
-			$this->tpl->draw("header");
+			if ($this->options["header"] === true) $this->tpl->draw("header");
 		}
 
 		private function setData($data = array())
@@ -52,7 +54,7 @@
 
 		public function __destruct() 
 		{
-		 	$this->tpl->draw("footer");
+		 	if ($this->options["footer"] === true) $this->tpl->draw("footer");
 		}
 	}
 ?>
